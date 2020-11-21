6d4ddc2fc9d3a5dbf91b4c0d244eea500b047609
Isha B <girllovestech93@gmail.com>
2020-11-21 15:04:03 -0800

    create new var (#8)

--- TheatreLink/TheatreLink/ViewController.swift
+++ TheatreLink/TheatreLink/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  var test = 3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
