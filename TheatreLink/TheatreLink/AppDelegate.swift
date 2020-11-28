1f9bc3fec217c863ca9873476798a43694d74bb8
Kauther Zeini <kautherz@gmail.com>
2020-11-27 20:42:35 -0600

    created login screen template (no functionality)

--- TheatreLink/TheatreLink/AppDelegate.swift
+++ TheatreLink/TheatreLink/AppDelegate.swift
@@ -6,14 +6,20 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        //let parseConfig = ParseClientConfiguration {
+        //            $0.applicationId = "DrbNJ1dewpYjy0PpWaQXBIcpuR8pXM1oLecktGQW"
+       //             $0.clientKey = "KNOGw5NRRXg84OYeFd8jKbZAx4QG8LLsO1W6g2tE"
+        //            $0.server = "https://parseapi.back4app.com"
+        //}
+       // Parse.initialize(with: parseConfig)
+    
         return true
     }
 
---
+++ TheatreLink/TheatreLink/LoginViewController.swift
@@ -0,0 +1,48 @@
+//
+//  LoginViewController.swift
+//  TheatreLink
+//
+//  Created by Kauther Zeini on 11/27/20.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onSignIn(_ sender: Any) {
+        
+        
+    }
+    
+    
+    
+    @IBAction func onSignUp(_ sender: Any) {
+        
+        
+    }
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
