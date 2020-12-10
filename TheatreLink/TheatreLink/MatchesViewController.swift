8239cfe6b3d72e66aad8b2a403e4f3749a2affed
Kauther Zeini <kautherz@gmail.com>
2020-12-10 00:32:14 -0600

    Finalized version of Matches Screen

--- TheatreLink/TheatreLink/MatchesViewController.swift
+++ TheatreLink/TheatreLink/MatchesViewController.swift
@@ -9,54 +9,27 @@ import UIKit
 import Parse
 import AlamofireImage
 
+struct Match {
+    let username: String
+    let movieID: Int
+    var movie = [String:Any]()
+}
+
+
 class MatchesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
     
-    var matches = [[String:Any]]() {
-        didSet {
-            tableView.reloadData() //whenever matches is set, reloads data and table view updates
-        }
-    }
+    
+    var movieMatches = [Match]()
+       
     
      override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        /*
-        1. list of all the movies you've swight right on
-        To do this: query SwipeDirection table for all that have user == yourUserId and swipe == right
-       
-        let currentUserRightSwipesQuery = PFQuery(className: "SwipeDirection")
-        currentUserRightSwipesQuery.whereKey("User", equalTo: PFUser.current()!)
-        currentUserRightSwipesQuery.whereKey("Swipe", equalTo:"right")
-        currentUserRightSwipesQuery.findObjectsInBackground { (objects : [PFObject]?, error:Error?) in
-          if let error = error {
-            print(error.localizedDescription)
-          } else {
-            print("\(objects?.count) objects found" )
-            
-          }
-        }
-         */
         
-        /*
-        2. find all swipe rights that aren't yours
-        to do this: query SwipeDirection table for all where user != yourUserId and swipe == right
-       
-        let otherUsersRightSwipesQuery = PFQuery(className: "SwipeDirection")
-        otherUsersRightSwipesQuery.whereKey("User", notEqualTo: PFUser.current()!)
-        otherUsersRightSwipesQuery.whereKey("Swipe", equalTo:"right")
-        otherUsersRightSwipesQuery.findObjectsInBackground { (objects : [PFObject]?, error:Error?) in
-          if let error = error {
-            print(error.localizedDescription)
-          } else {
-            print("\(objects?.count) objects found" )
-            
-          }
-        }
-         */
+        var matches = [Match]()
         
-        var matchingMovieIds = [Int]()
         /*
         1. list of all the movies you've swight right on
         To do this: query SwipeDirection table for all that have user == yourUserId and swipe == right
@@ -76,6 +49,7 @@ class MatchesViewController: UIViewController, UITableViewDataSource, UITableVie
                 let otherUsersRightSwipesQuery = PFQuery(className: "SwipeDirection")
                 otherUsersRightSwipesQuery.whereKey("User", notEqualTo: PFUser.current()!)
                 otherUsersRightSwipesQuery.whereKey("Swipe", equalTo:"right")
+                otherUsersRightSwipesQuery.includeKey("User")
                 otherUsersRightSwipesQuery.findObjectsInBackground { (otherLikedMovieIDs: [PFObject]?, error: Error?) in
                     /*
                     3. compare the two results for matching movie ids
@@ -88,17 +62,66 @@ class MatchesViewController: UIViewController, UITableViewDataSource, UITableVie
                           for otherLikedMovieID in otherLikedMovieIDs! {
                              // if movie id from object2 matches movie id from object 1, then append it to matchingMovieIds
                             let movieIDobj2 = otherLikedMovieID["MovieID"] as! Int
+                    
                             
-                            if ( movieIDobj1 == movieIDobj2){
-                                matchingMovieIds.append(movieIDobj2)
+                            if (movieIDobj1 == movieIDobj2){
+                                guard let username = (otherLikedMovieID["User"] as! PFUser).username else {
+                                    continue
+                                }
+                                
+                                let match = Match(username: username, movieID: movieIDobj2)
+                                
+                                let matchingMovieIds = matches.map {
+                                    $0.movieID
+                                }
+                                if !matchingMovieIds.contains(movieIDobj2) {
+                                    matches.append(match)
+                                }
+                                
                             }
                           }
+                        
                      }
+                    for (index, match) in matches.enumerated() {
+                        self.getMovieDetails(movieID: match.movieID, completion: { (movieObject) in
+                            let finalMatch = Match(username: match.username, movieID: match.movieID, movie: movieObject)
+                            self.movieMatches.append(finalMatch)
+                            if index == matches.count - 1  {
+                                self.tableView.reloadData()
+                            }
+                        })
+                        
+                    }
+                   
+                    
                 }
             }
         }
             
+    }
+    
+    func getMovieDetails(movieID: Int, completion: @escaping ([String:Any]) -> Void ){
         
+        // https://api.themoviedb.org/3/movie/577922?api_key=<<api_key>>&language=en-US
+        
+        let api = "https://api.themoviedb.org/3/movie/"
+      
+        let endpoint = "\(movieID)?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed&language=en-US"
+        let url = URL(string: api + endpoint)!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+           // This will run when the network request returns
+           if let error = error {
+              print(error.localizedDescription)
+           } else if let data = data {
+              let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+         
+            completion(dataDictionary)
+
+           }
+        }
+        task.resume()
         
     }
     
@@ -113,30 +136,18 @@ class MatchesViewController: UIViewController, UITableViewDataSource, UITableVie
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return matches.count
+        return movieMatches.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MatchesCell") as! MatchesCell
-        
-        //let match = matches[indexPath.row]
-        
-        //cell.usernameLabel?.text
-        //cell.movieNameLabel?.text=
+        let match = movieMatches[indexPath.row]
+        cell.usernameLabel?.text = match.username
+        cell.movieNameLabel?.text = match.movie["original_title"] as! String
         
         return cell
     }
     
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
