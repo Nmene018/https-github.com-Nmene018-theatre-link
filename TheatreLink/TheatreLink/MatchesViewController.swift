3c91c49c81633f81503596e5973f539bf7d75e34
Kauther Zeini <Kautherz@gmail.com>
2020-12-10 17:13:32 -0600

    current matches screen in progress (#27)
    * --current matches screen in progress
    
    * Finalized version of Matches Screen
    
    * The Clean and Finalized Version of the Matches Screen

---
+++ TheatreLink/TheatreLink/MatchesViewController.swift
@@ -0,0 +1,151 @@
+//
+//  MatchesViewController.swift
+//  TheatreLink
+//
+//  Created by Kauther Zeini on 12/7/20.
+//
+
+import UIKit
+import Parse
+import AlamofireImage
+
+struct Match {
+    let username: String
+    let movieID: Int
+    var movie = [String:Any]()
+}
+
+
+class MatchesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    var movieMatches = [Match]()
+       
+    
+     override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        getMatches()
+    }
+    
+    func getMovieDetails(movieID: Int, completion: @escaping ([String:Any]) -> Void ){
+        
+        // https://api.themoviedb.org/3/movie/577922?api_key=<<api_key>>&language=en-US
+        
+        let api = "https://api.themoviedb.org/3/movie/"
+      
+        let endpoint = "\(movieID)?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed&language=en-US"
+        let url = URL(string: api + endpoint)!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+           // This will run when the network request returns
+           if let error = error {
+              print(error.localizedDescription)
+           } else if let data = data {
+              let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+         
+            completion(dataDictionary)
+
+           }
+        }
+        task.resume()
+        
+    }
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+        getMatches()
+    }
+    
+    func getMatches(){
+        
+        var matches = [Match]()
+        
+        /*
+        1. list of all the movies you've swight right on
+        To do this: query SwipeDirection table for all that have user == yourUserId and swipe == right
+        */
+        let currentUserRightSwipesQuery = PFQuery(className: "SwipeDirection")
+        currentUserRightSwipesQuery.whereKey("User", equalTo: PFUser.current()!)
+        currentUserRightSwipesQuery.whereKey("Swipe", equalTo:"right")
+        currentUserRightSwipesQuery.findObjectsInBackground { (userLikedMovieIDs: [PFObject]?, error: Error?) in
+            if let error = error {
+                // Log details of the failure
+                print(error.localizedDescription)
+            } else if let userLikedMovieIDs = userLikedMovieIDs {
+                /*
+                2. find all swipe rights that aren't yours
+                to do this: query SwipeDirection table for all where user != yourUserId and swipe == right
+                */
+                let otherUsersRightSwipesQuery = PFQuery(className: "SwipeDirection")
+                otherUsersRightSwipesQuery.whereKey("User", notEqualTo: PFUser.current()!)
+                otherUsersRightSwipesQuery.whereKey("Swipe", equalTo:"right")
+                otherUsersRightSwipesQuery.includeKey("User")
+                otherUsersRightSwipesQuery.findObjectsInBackground { (otherLikedMovieIDs: [PFObject]?, error: Error?) in
+                    /*
+                    3. compare the two results for matching movie ids
+                    Add each match to a new list --nested for loop- outside list 1, inside list 2
+                    */
+                     for userLikedMovieID in userLikedMovieIDs {
+                          // get movie id from object1 -- MovieID column in Parse
+                        let movieIDobj1 = userLikedMovieID["MovieID"] as! Int
+                        
+                          for otherLikedMovieID in otherLikedMovieIDs! {
+                             // if movie id from object2 matches movie id from object 1, then append it to matchingMovieIds
+                            let movieIDobj2 = otherLikedMovieID["MovieID"] as! Int
+                    
+                            
+                            if (movieIDobj1 == movieIDobj2){
+                                guard let username = (otherLikedMovieID["User"] as! PFUser).username else {
+                                    continue
+                                }
+                                
+                                let match = Match(username: username, movieID: movieIDobj2)
+                                
+                                let matchingMovieIds = matches.map {
+                                    $0.movieID
+                                }
+                                if !matchingMovieIds.contains(movieIDobj2) {
+                                    matches.append(match)
+                                }
+                            }
+                          }
+                     }
+                    var finalMatches = [Match]()
+                    for (index, match) in matches.enumerated() {
+                        self.getMovieDetails(movieID: match.movieID, completion: { (movieObject) in
+                            let finalMatch = Match(username: match.username, movieID: match.movieID, movie: movieObject)
+                            finalMatches.append(finalMatch)
+                            if index == matches.count - 1  {
+                                self.movieMatches = finalMatches
+                                self.tableView.reloadData()
+                            }
+                        })
+                    }
+                }
+            }
+        }
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movieMatches.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MatchesCell") as! MatchesCell
+        let match = movieMatches[indexPath.row]
+        cell.usernameLabel?.text = match.username
+        cell.movieNameLabel?.text = match.movie["original_title"] as! String
+        
+        return cell
+    }
+    
+
+
+}
